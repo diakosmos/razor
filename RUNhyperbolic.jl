2449e1b06c979380194209298a221ceaa49fe597
diakosmos <58736428+diakosmos@users.noreply.github.com>
2021-09-15 10:26:03 -0700

    ping.

--- RUNhyperbolic.jl
+++ RUNhyperbolic.jl
@@ -1,7 +1,11 @@
 include("godunov.jl")
 using .Godunov
+using Plots
+fig = Plots.plot()
 r=Godunov.ring()
 Godunov.getAlphas!(r)
 for i in 1:100
+    Plots.plot!(r.X_,r.Σ_;legend=false)
+    display(fig)
     Godunov.step!(r)
 end

7cd37e4b4c59abee7b7b2bf7fea28e994e2f9648
diakosmos <58736428+diakosmos@users.noreply.github.com>
2021-09-17 06:47:59 -0700

    Pong.

--- RUNhyperbolic.jl
+++ RUNhyperbolic.jl
@@ -2,10 +2,21 @@ include("godunov.jl")
 using .Godunov
 using Plots
 fig = Plots.plot()
-r=Godunov.ring()
+NN = 100 # no of cells
+Nt = 64000000 # no of time steps
+Np = 6400000 # how often to plot
+Ns = 5000 # when to start plotting
+r=Godunov.ring(NN,xi=0.2)
 Godunov.getAlphas!(r)
-for i in 1:100
-    Plots.plot!(r.X_,r.Σ_;legend=false)
-    display(fig)
+for i in 1:Nt
+    if (mod(i,Np)==0) & (i>Ns)
+        t = r.t
+        Plots.plot!(r.X_[1:NN÷1],r.Σ_[1:NN÷1],label="t: $t",legend=:bottomright)#,title="Σ")#;legend=false)
+        display(fig)
+        Plots.plot!(r.X_[1:NN÷1],r.J_[1:NN÷1],label=false)#,label="t: $t",legend=:topleft)#,title="J")#;legend=false)
+        display(fig)
+    end
     Godunov.step!(r)
 end
+
+print("Done!")
--- godunov.jl
+++ godunov.jl
@@ -5,7 +5,7 @@ using LinearAlgebra
     For now, we assume a constant (fixed) viscosity "ν".
 """
 
-ν = 1.0
+ν = 100.0
 s = 1.0 # relaxation time
 q = 3*ν/s
 
@@ -31,6 +31,7 @@ mutable struct ring
     Λ₋L::Any                #
     α⁺::Array{Float64,1}    #
     α⁻::Array{Float64,1}    #
+    t::Float64              # time
 end
 function ring(N=5;β=0,xf=3.0,xi=0.01)#10*xf/N)
     function setEs(U_::Array{Float64,1})
@@ -43,10 +44,10 @@ function ring(N=5;β=0,xf=3.0,xi=0.01)#10*xf/N)
             λ₊[i] = U_[i] + √q
             λ₋[i] = U_[i] - √q
         end
-        Λ₊R = Diagonal(λ₊ .* (λ₊ .> 0))
-        Λ₊L = Diagonal(λ₊ .* (λ₊ .< 0))
-        Λ₋R = Diagonal(λ₋ .* (λ₋ .> 0))
-        Λ₋L = Diagonal(λ₋ .* (λ₋ .< 0))
+        Λ₊R =  Diagonal(λ₊ .* (λ₊ .> 0))
+        Λ₊L = -Diagonal(λ₊ .* (λ₊ .< 0))
+        Λ₋R =  Diagonal(λ₋ .* (λ₋ .> 0))
+        Λ₋L = -Diagonal(λ₋ .* (λ₋ .< 0))
         return (r₊,r₋,λ₊,λ₋,Λ₊R,Λ₊L,Λ₋R,Λ₋L)
     end
     xtemp_ = collect(range(xi,xf,length=N+1))
@@ -60,7 +61,7 @@ function ring(N=5;β=0,xf=3.0,xi=0.01)#10*xf/N)
     Δt = 0.5 / max( maximum(abs.(λ₊./ΔX_)), maximum(abs.(λ₋./ΔX_))) * FUDGE
     α⁺=zeros(N)
     α⁻=zeros(N)
-    return ring(N,β,Δt,X_,ΔX_,U_,Σ_,J_,r₊,r₋,λ₊,λ₋,Λ₊R,Λ₊L,Λ₋R,Λ₋L,α⁺,α⁻)
+    return ring(N,β,Δt,X_,ΔX_,U_,Σ_,J_,r₊,r₋,λ₊,λ₋,Λ₊R,Λ₊L,Λ₋R,Λ₋L,α⁺,α⁻,0.0)
 end
 
 """
@@ -78,9 +79,11 @@ function getAlphas!(r::ring)
             ΔΣ = 1 - Σ_[i]
             ΔJ = 0.0
         end
-        print(0.5 * [1  1/√q] ⋅ [ΔΣ, ΔJ])
-        α⁺[i] = 0.5 * [1  1/√q] ⋅ [ΔΣ, ΔJ]
-        α⁻[i] = 0.5 * [1 -1/√q] ⋅ [ΔΣ, ΔJ]
+        #print(0.5 * [1  1/√q] ⋅ [ΔΣ, ΔJ])
+    #=    α⁺[i] = 0.5 * [1  1/√q] ⋅ [ΔΣ, ΔJ]
+        α⁻[i] = 0.5 * [1 -1/√q] ⋅ [ΔΣ, ΔJ]  =#
+        α⁺[i] = [ √q , 1] ⋅ [ΔΣ, ΔJ] ./ √(1+q)
+        α⁻[i] = [ √q ,-1] ⋅ [ΔΣ, ΔJ] ./ √(1+q)
     end
 end#function
 
@@ -88,14 +91,41 @@ function step!(r::ring)
     getAlphas!(r)
     Δt=r.Δt; Σ_=r.Σ_; J_=r.J_; α⁺=r.α⁺; α⁻=r.α⁻; r₋=r.r₋; r₊=r.r₊
     Λ₊L=r.Λ₊L; Λ₊R=r.Λ₊R; Λ₋L=r.Λ₋L; Λ₋R=r.Λ₋R
+    N = length(Σ_)
+
     Σ_[1:end  ] += Δt * ((Λ₋L * α⁻ * r₋[1])                     )[1:end  ]
     Σ_[1:end  ] += Δt * (                     (Λ₊L * α⁺ * r₊[1]))[1:end  ]
-    Σ_[2:  end] += Δt * ((Λ₋R * α⁻ * r₋[1])                     )[1:end-1]
-    Σ_[2:  end] += Δt * (                     (Λ₊R * α⁺ * r₊[1]))[1:end-1]
+    Σ_[2:  end] += Δt * (-(Λ₋R * α⁻ * r₋[1])                     )[1:end-1]
+    Σ_[2:  end] += Δt * (                     -(Λ₊R * α⁺ * r₊[1]))[1:end-1]
+    #Σ_ = Σ_.* (Σ_ .> 0)  # sanity - kills instability?
     J_[1:end  ] += Δt * ((Λ₋L * α⁻ * r₋[2])                     )[1:end  ]
-    J_[1:end  ] += Δt * (                     (Λ₊L * α⁺ * r₊[2]))[1:end  ]
-    J_[2:  end] += Δt * ((Λ₋R * α⁻ * r₋[2])                     )[1:end-1]
-    J_[2:  end] += Δt * (                     (Λ₊R * α⁺ * r₊[2]))[1:end-1]
+    J_[1:end  ] += Δt * (                      (Λ₊L * α⁺ * r₊[2]))[1:end  ]
+    J_[2:  end] += Δt * (-(Λ₋R * α⁻ * r₋[2])                     )[1:end-1]
+    J_[2:  end] += Δt * (                     -(Λ₊R * α⁺ * r₊[2]))[1:end-1]
+    #=
+    #Threads.@threads for i in 1:N
+    @simd for i in 1:N
+        if i<N
+            Σ_[i] += Δt * ((Λ₋L[i] * α⁻[i] * r₋[1])                     )
+            Σ_[i] += Δt * (                     (Λ₊L[i] * α⁺[i] * r₊[1]))
+        end
+        if i>1
+            Σ_[i] += Δt * (-(Λ₋R[i-1] * α⁻[i-1] * r₋[1])                     )
+            Σ_[i] += Δt * (                     -(Λ₊R[i-1] * α⁺[i-1] * r₊[1]))
+        end
+        if i<N
+            J_[i] += Δt * ((Λ₋L[i] * α⁻[i] * r₋[2])                     )
+            J_[i] += Δt * (                     (Λ₊L[i] * α⁺[i] * r₊[2]))
+        end
+        if i>1
+            J_[i] += Δt * (-(Λ₋R[i-1] * α⁻[i-1] * r₋[2])                     )
+            J_[i] += Δt * (                     -(Λ₊R[i-1] * α⁺[i-1] * r₊[2]))
+        end
+    r.Σ_[i] = Σ_[i]
+    r.J_[i] = J_[i]
+    end
+    =#
+    r.t += Δt
 end
 
 end#module
